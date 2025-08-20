1145d1165d5121f5189714fb0ee2023d52a7d0d9
admin_dho <davidhorph@hotmail.com>
2025-08-20 00:54:02 -0500

    .__len__ method and .addItem
    Updated Store class so that it takes Food (class Food) items.
    
    Validation included.
    
    Also has a .addItem to add items and .__len__ so you can get the length (otherwise, you cannot do len(Store Instance)).

--- classes/store.py
+++ classes/store.py
@@ -12,6 +12,52 @@ class Food:
    def __repr__(self):
       return f"<New Food Item: {self.name} {self.category} {self.price} >"
 
+class Store():
+   def __init__(self, *foods):
+      nonFood = list(filter(lambda x: not isinstance(x, Food), foods)); #filters out values that are not type Food.
+      try:
+         if len(nonFood):
+            self.inventory = []
+            raise TypeError(f"{nonFood} are not instances of Food!!! They are all {[type(x) for x in nonFood]}.") #Throws error if any item is not type Food.
+         else:
+            self.foods = foods;
+            self.inventory = [*self.foods];
+      except TypeError as TE:
+         print(TE)
+   
+   def __repr__(self):
+      return f"<Store inventory {self.inventory}>"
+
+   def addItem(self, foods:list): # Method to add new food.
+      try:
+         for i in foods:
+            if not isinstance(i, Food):
+               raise Exception(f"{i} is {type(i)} and NOT type Food.")
+            else:
+               self.inventory.append(i)
+         return self.inventory;
+      except Exception as EXC:
+         print(f"AN ERROR OCCURRED => {EXC}!!!!!")
+         return f"EXCEPTION WARNING!!! {Exception}."
+   
+   def __len__(self): # Without this, I cannot do len(Store Instance). len calls this function.
+      return len(self.inventory) # It does not make sense, but I can add 23 to the return and len(Store Instance) would be the result of len(self.inventory) + 23.
+
+MyStore = Store(Food('Bologna', 'meat', 1.99), Food('sardines', 'other', 7.99), Food('lemonade', 'beverage', 5.99), Food('avocado', 'vegetable', 1.99), Food('water', 'beverage', 0.99));
+
+MyStore.addItem([Food('bacon', 'meat', 7.45), Food('pineapples', 'fruit', 5.99), Food('rasberries', 'fruit', 3.99)])
+
+print(len(MyStore))
+
+
+
+
+
+      
+
+
+      
+
 
 
 
