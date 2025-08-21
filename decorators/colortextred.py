28c68410a49dee98fde124e209c07a1b5691c92f
admin_dho <davidhorph@hotmail.com>
2025-08-21 15:15:48 -0500

    comments.

--- decorators/colortextred.py
+++ decorators/colortextred.py
@@ -4,10 +4,10 @@ def textcolor_red(fn):
    from colorama import init;
    from termcolor import colored;
    @wraps(fn)
-   def wrapper(text):
+   def wrapper(text): #the text is what is passed into the fn below.
       init();
       return colored(f"{'*'*3}{fn(text)}{'*'*3}", 'red', attrs=['bold'])
-   return wrapper
+   return wrapper #It is the WRAPPER declaration that is retured.
 
 @textcolor_red
 def sometext(text):
