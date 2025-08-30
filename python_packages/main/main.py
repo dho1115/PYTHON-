bbea8f1984a177428b66798869697c049a63e2e0
admin_dho <davidhorph@hotmail.com>
2025-08-30 17:05:22 -0500

    Updated Python Package.
    Removed the main folder.

--- python_packages/main/main.py
+++
@@ -1,9 +0,0 @@
-import sys;
-import os;
-
-# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))); #not recommended.
-from math_package import addition, subtraction, multiply;
-
-print(addition)
-
-
