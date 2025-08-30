93fe0f01a8748d193635be43f6dc55f49df3e259
admin_dho <davidhorph@hotmail.com>
2025-08-30 16:32:43 -0500

    Commented out not recommended way.

--- python_packages/main/main.py
+++ python_packages/main/main.py
@@ -1,7 +1,7 @@
 import sys;
 import os;
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))); #not recommended.
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))); #not recommended.
 from math_package import addition, subtraction, multiply;
 
 print(addition)
