1a12d4cfad082c5404ab101b86be272945805084
admin_dho <davidhorph@hotmail.com>
2025-08-30 16:32:09 -0500

    Create main.py
    Where to run the program.
    NOTE: sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))); is NOT recommended.

---
+++ python_packages/main/main.py
@@ -0,0 +1,9 @@
+import sys;
+import os;
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))); #not recommended.
+from math_package import addition, subtraction, multiply;
+
+print(addition)
+
+
