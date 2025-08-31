cd50cf0808584cbf35c255369c4567f8fe3e940c
admin_dho <davidhorph@hotmail.com>
2025-08-31 14:17:01 -0500

    Logic to return (first 5) closing dates, values.
    filteredResult returns the data you see in sampledata.txt:
    
    first5dates
    first5values
    first5ClosingValues.

--- projects/stock_tracker/StockTracker.py
+++ projects/stock_tracker/StockTracker.py
@@ -1,14 +1,27 @@
 if __name__ == "__main__":
+   '''
+   WARNING!!!
+   The api I use, https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=go-get-your-own-damn-api-stop-using-mine-I-need-to-save-my-call-limit!!!
+
+   ONLY ALLOWS LIKE, 25 'F*CKIN' API CALLS A DAY OR SOME STUPID SH*T LIKE THAT (D*MN A...........E CHEAPOS)!!!!!
+
+   So (ahem)... anyway, it goes to say that, if you go get your own api-key (you don't have to, but if you don't you get ONE MEASLY FREE DEMO. That's it... or something like that), just be careful.
+
+   This is why I hide my api-key inside a dot-env (that and it's good practice).
+   '''
    try:
-      from functions import all_data, filteredData, link
+      from functions import all_data, filteredData, link, regularExpressionSearch
 
       ticker = input("Enter a Ticker Symbol: ")
 
-      filteredResult = filteredData(all_data, link(ticker));
+      filteredResult = (filteredData(all_data, link(ticker))['data'])
 
-      print(filteredResult)
+      print(filteredResult)     
+
    except Exception as EXC:
       print("EXCEPTION: ", EXC)
+
+
 
 
 
