4c22cc876cc7710433383d4992b55966645e73e8
admin_dho <davidhorph@hotmail.com>
2025-09-01 13:25:08 -0500

    How to unwrap a decorater: @wraps(fn).
    @wraps(fn) is like a decorator that you put INSIDE of a decorator that removes the "effect(s)" of the parent decorator... so to speak.

--- decorators/colortextred.py
+++ decorators/colortextred.py
@@ -3,7 +3,7 @@ from functools import wraps;
 def textcolor_red(fn): # This is the DECORATOR!!!
    from colorama import init;
    from termcolor import colored;
-   @wraps(fn)
+   @wraps(fn) # Not really necessary... BUT if you ever want to unwrapp a function, you kinda, sorta need this ("kinda, sorta" because there IS another way to unwrap a function... but I forgot what it is :(:( :(.)
    def wrapper(text): #the text is what is passed into the fn below.
       init();
       return colored(f"{'*'*3}{fn(text)}{'*'*3}", 'red', attrs=['bold'])
