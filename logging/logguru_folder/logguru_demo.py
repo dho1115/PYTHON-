6ce31298ed5da978caa77893b6e1b96db7f1a2b5
admin_dho <davidhorph@hotmail.com>
2025-09-06 08:31:08 -0500

    New Logs.

--- logging/logguru_folder/logguru_demo.py
+++ logging/logguru_folder/logguru_demo.py
@@ -10,7 +10,13 @@ if __name__ == "__main__":
       logger.info("This is the loguru_demo!!!") #Ran inside of the __main__ module, so it would show up as __main__:<module>: 5 (line 5).
 
       def runLoggerInfo(msg:str):
-         return logger.info(msg) #Ran inside of a runLoggerInfo function, so it returns as __main__:runLoggerInfo:8 
+         return logger.info(msg) #Ran inside of a runLoggerInfo function, so it returns as __main__:runLoggerInfo:8
+      
+      def getChildLogger(**kwargs):
+         myChildLogger = logger.bind(**kwargs)
+         return myChildLogger.info("PERSONNEL PROFILE:")
+      
+      getChildLogger(_id="js12345", name="Jamie Smith", role="asst. manager")
 
       runLoggerInfo("This is inside of a function!!!")
 
