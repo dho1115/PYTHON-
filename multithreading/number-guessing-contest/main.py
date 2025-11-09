e3675fda8a9eee73d0c16d054f7ffe1fd76919f3
admin_dho <davidhorph@hotmail.com>
2025-11-09 14:22:31 -0600

    Added input( ) in the main thread (main.jsx).
    To "block" the main thread from terminating so the daemon thread in functions.py can run.

--- multithreading/number-guessing-contest/main.py
+++ multithreading/number-guessing-contest/main.py
@@ -5,4 +5,5 @@ if __name__ == "__main__":
 
    with contest(contestant_props, correctNumber) as c:
       print(c);
-      print("\n")
+      
+   stopThread = input("Press Enter to Stop...."); # I created the stopThread on this main thread (main.py is the 'main thread') so I can terminate this program (terminate the threads) if the "contestant" threads (the threads in allThreads) take to damn long to guess. NOTE: In order for the threads in allThreads to terminate when I hit "Enter", they must all be DAEMON threads.
